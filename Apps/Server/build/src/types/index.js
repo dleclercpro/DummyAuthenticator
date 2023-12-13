bce2bb2be74860b82a1f8515586325d5051ac85b
David Leclerc <d.leclerc.pro@gmail.com>
2023-12-13 07:14:39 +0100

    No build files on GitHub.

--- Apps/Server/build/src/types/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Environment = void 0;
-require("./RequestTypes"); // Do not remove!
-var Environment;
-(function (Environment) {
-    Environment["Development"] = "development";
-    Environment["Production"] = "production";
-})(Environment = exports.Environment || (exports.Environment = {}));
