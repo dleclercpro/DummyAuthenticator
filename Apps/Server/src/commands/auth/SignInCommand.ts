1b312b9c39a124e130ca162db92eadc0d406b21f
David Leclerc <d.leclerc.pro@gmail.com>
2024-04-24 07:43:41 +0200

    Fix: store all login attempts.

--- Apps/Server/src/commands/auth/SignInCommand.ts
+++ Apps/Server/src/commands/auth/SignInCommand.ts
@@ -35,24 +35,26 @@ class SignInCommand extends Command<Argument, Response> {
         // Try and find user in database
         const user = await new GetUserCommand({ email }).execute();
 
-        // Get last login attempt
+        // Get failed login attempts in the last hour
         const oneHourAgo = computeTime(new Date(), new TimeDuration(-1, TimeUnit.Hour));
         const failedLoginAttemptsInLastHour = user.getLogin().getAttempts()
             .filter(attempt => attempt.type === LoginAttemptType.Failure)
             .filter(attempt => attempt.timestamp > oneHourAgo);
 
+        // Authenticate user
+        const isPasswordValid = await PasswordManager.isValid(password, user.getPassword().getValue());
+
+        // Store login attempt
+        user.getLogin()
+            .addAttempt(isPasswordValid ? LoginAttemptType.Success : LoginAttemptType.Failure);
+        
+        await user.save();
+
         // Only allow X failed login attempts per hour
         if (failedLoginAttemptsInLastHour.length > HOURLY_LOGIN_ATTEMPT_MAX_COUNT) {
             throw new ErrorNoMoreLoginAttempts(user.getEmail(), failedLoginAttemptsInLastHour.length);
         }
-
-        // Authenticate user
-        const isPasswordValid = await PasswordManager.isValid(password, user.getPassword().getValue());
         
-        // Store login attempt
-        user.getLogin().addAttempt(isPasswordValid ? LoginAttemptType.Success : LoginAttemptType.Failure);
-        await user.save();
-
         if (!isPasswordValid) {
             throw new ErrorUserWrongPassword(user);
         }
