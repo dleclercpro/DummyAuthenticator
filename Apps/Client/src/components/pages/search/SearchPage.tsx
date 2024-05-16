6bf6aea8a615936819f64469dc0b5e3043b3c65b
David Leclerc <d.leclerc.pro@gmail.com>
2024-05-16 12:27:04 +0200

    Feat: table to list search results.

--- Apps/Client/src/components/pages/search/SearchPage.tsx
+++ Apps/Client/src/components/pages/search/SearchPage.tsx
@@ -1,10 +1,15 @@
-import { Paper, TextField, Typography } from '@mui/material';
+import { Button, Paper, TextField, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { Severity } from '../../../types/CommonTypes';
 import useAuthPageStyles from '../AuthPageStyles';
 import Snackbar from '../../Snackbar';
 import SearchIcon from '@mui/icons-material/Search';
+import BackIcon from '@mui/icons-material/ArrowBack';
 import LoadingButton from '../../buttons/LoadingButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Page, getURL } from '../../../routes/Router';
+import { Link } from 'react-router-dom';
+import useDatabase from '../../../hooks/useDatabase';
 
 interface Props {
 
@@ -18,8 +23,31 @@ const SearchPage: React.FC<Props> = () => {
 
     const [isSearching, setIsSearching] = useState(false);
 
+    const [userEmail, setUserEmail] = useState('');
+
     const [value, setValue] = useState('');
     const [error, setError] = useState(false);
+
+    const [version, setVersion] = useState(0);
+
+    const [isDeleteUserConfirmDialogOpen, setIsDeleteUserConfirmDialogOpen] = useState(false);
+
+    const openDeleteUserConfirmDialog = (email: string) => {
+        setIsDeleteUserConfirmDialogOpen(true);
+    }
+    const closeDeleteUserConfirmDialog = () => {
+        setIsDeleteUserConfirmDialogOpen(false);
+    }
+
+    const handleDeleteUser = async () => {
+        setIsDeleteUserConfirmDialogOpen(false);
+
+        await deleteUser(userEmail);
+
+        setVersion(version + 1);
+    }
+
+    const { users, admins, isDeletingUser, deleteUser } = useDatabase();
 
     useEffect(() => {
 
@@ -72,6 +100,87 @@ const SearchPage: React.FC<Props> = () => {
                     >
                         Search
                     </LoadingButton>
+                </div>
+
+                <div className={`${classes.form} users`}>
+                    {admins.length > 0 && (
+                        <>
+                            <Typography className={classes.text}>
+                                Here is the list of admin users:
+                            </Typography>
+
+                            <table className={classes.table}>
+                                {admins.map((email) => (
+                                    <tr>
+                                        <td>
+                                            <Typography>
+                                                <strong>{`${email.value} ${email.confirmed ? '✅' : '❌'}`}</strong>
+                                            </Typography>
+                                        </td>
+                                        <td>
+                                            <LoadingButton
+                                                className={classes.button}
+                                                variant='contained'
+                                                color='error'
+                                                icon={<DeleteIcon />}
+                                                loading={isDeletingUser && email.value === userEmail}
+                                                disabled
+                                                onClick={() => openDeleteUserConfirmDialog(email.value)}
+                                            >
+                                                Delete
+                                            </LoadingButton>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </table>
+                        </>
+                    )}
+                    
+                    {users.length > 0 && (
+                        <>
+                            <Typography className={classes.text}>
+                                Here is the list of regular users:
+                            </Typography>
+
+                            <table className={classes.table}>
+                                {users.map((email) => (
+                                    <tr>
+                                        <td>
+                                            <Typography>
+                                                <strong>{`${email.value} ${email.confirmed ? '✅' : '❌'}`}</strong>
+                                            </Typography>
+                                        </td>
+                                        <td>
+                                            <LoadingButton
+                                                className={classes.button}
+                                                variant='contained'
+                                                color='error'
+                                                icon={<DeleteIcon />}
+                                                loading={isDeletingUser && email.value === userEmail}
+                                                onClick={() => openDeleteUserConfirmDialog(email.value)}
+                                            >
+                                                Delete
+                                            </LoadingButton>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </table>
+                        </>
+                    )}
+                </div>
+
+                <div className={classes.buttons}>
+                    <div className='top'>
+                        <Button
+                            className={classes.linkButton}
+                            component={Link}
+                            to={getURL(Page.Home)}
+                            color='secondary'
+                            startIcon={<BackIcon />}
+                        >
+                            Back
+                        </Button>
+                    </div>
                 </div>
 
                 <Snackbar
